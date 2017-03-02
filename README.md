74037cfec08eb4e3ed3bfae17c63b35439cd952f
Giovani Aguirre <aguirregio19@gmail.com>
2017-03-01 19:34:53 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GoodToGoReactNSass
+A Boiler Plate for Reaact.js and Sass with instructions in index.html
---
+++ src/js/components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Footer from "./Footer";
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Good To Go!</h1>
+        <Footer />
+      </div>
+  )}
+}
